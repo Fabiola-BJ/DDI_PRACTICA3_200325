1d5bb193b9e8e7ff221013c5558eace736fd47f1
Fabiola-BJ <fabiola.bautistajoaquin@utxicotepec.edumnx>
2023-07-21 12:58:44 -0600

    practica 3

---
+++ app/src/main/java/mx/edu/utxj/ti/idgs/ddi/practica3_200325/presentation/theme/Color.kt
@@ -0,0 +1,21 @@
+package mx.edu.utxj.ti.idgs.ddi.practica3_200325.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.wear.compose.material.Colors
+
+val Purple200 = Color(0xFF89FF00)
+val Purple500 = Color(0xFF89FF00)
+val Purple700 = Color(0xFF89FF00)
+val Teal200 = Color(0xFF89FF00)
+val Red400 = Color(0xFF89FF00)
+
+internal val wearColorPalette: Colors = Colors(
+        primary = Purple200,
+        primaryVariant = Purple700,
+        secondary = Teal200,
+        secondaryVariant = Teal200,
+        error = Red400,
+        onPrimary = Color.Black,
+        onSecondary = Color.Black,
+        onError = Color.Black
+)
